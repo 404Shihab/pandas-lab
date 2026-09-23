696f2e897bee026245d47eba431269e155e70eeb
404Shihab <shihabbhuiyan500@gmail.com>
2026-09-23 10:27:36 +0600

    Refactor aggregation script to include comprehensive statistics for the dataset

--- Aggregation/main.py
+++ Aggregation/main.py
@@ -7,4 +7,29 @@ import pandas as pd
 
 df = pd.read_csv("Aggregation/data.csv")
 
-print(df.to_string())
+# print(df.to_string())
+
+# --- whole data frame ----
+
+print(df.mean(numeric_only=True))
+print(df.sum(numeric_only=True))
+print(df.min(numeric_only=True))
+print(df.max(numeric_only=True))
+print(df.count())
+
+
+# --- Single column ---
+
+print(df["Age"].mean())
+print(df["StrikeRate"].min())
+print(df["Runs"].max())
+print(df["Matches"].count())
+
+# ---groupby() ---
+
+group = df.groupby("Country")
+print(group["Runs"].mean())
+print(group["Runs"].sum())
+print(group["Wickets"].min())
+print(group["Wickets"].max())
+print(group["Matches"].count())
